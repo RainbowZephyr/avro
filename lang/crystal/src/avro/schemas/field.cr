d9e99d166000829db39683196b4c2dfc8db2d42b
RainbowZephyr <darkfeathers9@gmail.com>
2024-04-04 05:01:18 +0200

    fix: handling variable type issues

--- lang/crystal/src/avro/schemas/field.cr
+++ lang/crystal/src/avro/schemas/field.cr
@@ -1,18 +1,20 @@
 require "./abstract_schema"
-
+require "../schema_validator"
 module Avro::Schemas
   class Field < AbstractSchema
-    getter :type, :name, :default, :order, :doc, :aliases
+    getter :field_type, :name, :default, :order, :doc, :aliases
+    @field_type : Avro::Schemas::AbstractSchema
+    @name : String
 
-    def initialize(type : String, name, default = :no_default, order : String? = nil, names : Hash(String, Avro::Schemas::AbstractSchema) = nil, namespace : String? = nil, doc : String? = nil, aliases : Array(String)? = nil)
+    def initialize(type : String, name : String, default : String = "no_default", order : String? = nil, names : Hash(String, Avro::Schemas::AbstractSchema) = nil, namespace : String? = nil, doc : String? = nil, aliases : Array(String)? = nil)
       super(type, nil)
-      @type = subparse(type, names, namespace)
+      @field_type = subparse(type, names, namespace)
       @name = name
       @default = default
       @order = order
       @doc = doc
       @aliases = aliases
-      @type_adapter = nil?
+      @type_adapter = nil
       validate_aliases! if aliases
       validate_default! if default? && !Avro.disable_field_default_validation
     end
@@ -34,16 +36,18 @@ module Avro::Schemas
     end
 
     private def validate_default!
-      type_for_default = if type.type_sym == :union
-                           type.schemas.first
-                         else
-                           type
-                         end
+      type_for_default : Avro::Schemas::AbstractSchema
+      if type == "union"
+        type_for_default = @field_type.as(Avro::Schemas::UnionSchema).schemas.first
+      else
+        type_for_default = @field_type
+      end
+
       case type_for_default.logical_type
       when DECIMAL_LOGICAL_TYPE
         # https://avro.apache.org/docs/1.11.1/specification/#schema-record
         # Default values for bytes and fixed fields are JSON strings, where Unicode code points 0-255 are mapped to unsigned 8-bit byte values 0-255
-        options = SchemaValidator::DEFAULT_VALIDATION_OPTIONS.dup
+        options = Avro::SchemaValidator::DEFAULT_VALIDATION_OPTIONS.dup
         options[:encoded] = true
         Avro::SchemaValidator.validate!(type_for_default, default, options)
       else
